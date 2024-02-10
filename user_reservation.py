ba6ebebde9b2d18cb08affcc675b674d710e6304
SebekMal <sebastian.malolepszy1@gmail.com>
2024-02-10 16:53:02 +0100

    Changed format files and create function which check date during reservation process.

--- user_reservation.py
+++ user_reservation.py
@@ -1,6 +1,4 @@
-from clients_db import saving_clients_data
-from desk_database_checker import reading_database, choosing_desk
-from userdata_input import user_data_input
+import json
 from datetime import datetime
 
 
@@ -45,7 +43,7 @@ def user_reservation_module():
 
 
     def size_choosing():
-        print("Ile stanowisk Cię interesuje?: ")
+        print("Jakie biurko Cię interesuje?: ")
         print("Biurko 1-osobowe - wprowadź cyfrę 1")
         print("Biurko 4-osobowe - wprowadź cyfrę 4")
         print("Biurko 10-osobowe - wprowadź cyfrę 10")
@@ -94,7 +92,6 @@ def user_reservation_module():
 
         elif int(user_choice_size) == 1 and int(user_choice_display) == 2:
             print("Prezentuję ofertę dostępnych biurek 1-osobowych bez monitora")
-            reading_database('desks.json')
             rozmiar = 1
             monitor = False
             return rozmiar, monitor
@@ -123,25 +120,171 @@ def user_reservation_module():
             monitor = False
             return rozmiar, monitor
     size, display = presenting_avaliability()
-    reading_database('desks.json', size, display)
+
+    def checking_availiability_by_date(filename='user_data.json', user_beginning_date_str='', user_end_date_str=''):
+        with open(filename, 'r') as file:
+            data = json.load(file)
+
+        user_beginning_date = datetime.strptime(user_beginning_date_str, "%Y-%m-%d")
+        user_end_date = datetime.strptime(user_end_date_str, "%Y-%m-%d")
+
+        reserved_desks = []
+
+        for id_reservation, reservation_spec in data.items():
+            reservation_start = datetime.strptime(reservation_spec['Wynajem od'], "%Y-%m-%d")
+            reservation_end = datetime.strptime(reservation_spec['Wynajem do'], "%Y-%m-%d")
+
+            if not (user_end_date <= reservation_start or user_beginning_date >= reservation_end):
+                reserved_desks.append(reservation_spec['Biurko'])
+
+        return reserved_desks
+    reserved_desks = checking_availiability_by_date(user_beginning_date_str=user_beginning_date_str, user_end_date_str=user_end_date_str)
+
+    def choosing_desk_v2(exclude_desk_ids=None, size=None, display=None):
+        with open('desks.json', 'r') as file:
+            data = json.load(file)
+
+        if exclude_desk_ids is None:
+            exclude_desk_ids = []
+
+        for desk_name, desk_info in data.items():
+            if desk_name not in exclude_desk_ids and (size == desk_info['rozmiar'] and display == desk_info['monitor']):
+                print(f"Numer biurka: {desk_name}:")
+                print(f"Specyfikacja: {desk_info['desk_type']}, "
+                      f"\nCena za jedno stanowisko: {desk_info['price']} PLN")
+                if desk_info['rozmiar'] > 1:
+                    print(f"Całkowita cena wynajmu biurka: {desk_info['price'] * desk_info['rozmiar']}")
+                print("")
+            else:
+                continue
+    choosing_desk_v2(exclude_desk_ids=reserved_desks, size=size, display=display)
 
     print("Oto lista dostępnych biurek.")
     user_desk_choice = input("Wprowadź numer wybranego przez siebie biurka: ")
-    desk_price, desk_name = choosing_desk(user_desk_choice)
 
-    def value_calculating(desk_price, days_counter):
-        total_value = desk_price*days_counter
+    def choosing_desk_from_displayed(user_desk_choice):
+        with open('desks.json', 'r') as file:
+            data = json.load(file)
+
+        user_desk_choice = str(user_desk_choice)
+
+        for desk_name, desk_info in data.items():
+            if user_desk_choice == desk_name:
+                print(f'Wybrano biurko o numerze: {desk_name}')
+                print(f'Specyfikacja: {desk_info["desk_type"]}')
+                print(f'Cena: {desk_info["price"]} PLN za 1 miejsce')
+                return int(desk_info["price"]), desk_name, (desk_info["desk_type"])
+    desk_price, desk_name, desk_type = choosing_desk_from_displayed(user_desk_choice)
+
+    def value_calculating(desk_price, days_counter, size):
+        total_value = desk_price*size*days_counter
         print(f"Wybrałeś biurko nr: {desk_name}, całkowita cena wynajmu wyniesie {total_value} PLN.")
         return total_value
-    total_value = value_calculating(desk_price, days_counter)
+    total_value = value_calculating(desk_price, days_counter, size)
 
-    def confirming_imputed_data(desk_name, user_beginning_date_str, user_end_date_str, total_value):
+    def saving_clients_data(name, surname, phone, email, desk_name, desk_type,
+                            user_beginning_date_str, user_end_date_str, total_value):
+        try:
+            with open('user_data.json', 'r') as file:
+                data = json.load(file)
+                reservation_id = max([int(k) for k in data.keys()]) + 1 if data else 1
+        except (FileNotFoundError, json.JSONDecodeError):
+            data = {}
+            reservation_id = 1
+
+        data[str(reservation_id)] = {
+            "Imie": name,
+            "Nazwisko": surname,
+            "Telefon": phone,
+            "Email": email,
+            "Biurko": desk_name,
+            "Specyfikacja": desk_type,
+            "Wynajem od": user_beginning_date_str,
+            "Wynajem do": user_end_date_str,
+            "Calkowita wartosc wynajmu": total_value
+        }
+
+        with open('user_data.json', 'w') as file:
+            json.dump(data, file, indent=4, sort_keys=False)
+
+        return reservation_id
+
+    def user_data_input():
+        print("Wprowadź dane osobowe")
+        valid_choice = True
+        while valid_choice:
+
+            def name_insert():
+                valid_name = True
+                while valid_name:
+                    name = input("Podaj swoje imie: ")
+                    if name.isalpha():
+                        return name
+                        valid_name = False
+                    else:
+                        print("Podaj poprawne imię")
+
+            name = name_insert()
+
+            def surname_insert():
+                valid_surname = True
+                while valid_surname:
+                    surname = input("Podaj swoje nazwisko: ")
+                    if surname.isalpha():
+                        return surname
+                        valid_surname = False
+                    else:
+                        print("Podaj poprawne nazwisko")
+
+            surname = surname_insert()
+
+            def valid_phone_insert():
+                valid_phone = True
+                while valid_phone:
+                    phone = input("Podaj numer telefonu liczbami bez kierunkowego (9 cyfr): ")
+                    if phone.isdigit() and len(phone) == 9:
+                        return phone
+                        valid_phone = False
+                    else:
+                        print("Podaj poprawny numer telefonu")
+
+            phone = valid_phone_insert()
+
+            def valid_email_insert():
+                valid_mail = True
+                while valid_mail:
+                    email = input("Podaj swoj adres e-mail: ")
+                    if '@' in email and '.' in email and len(email) >= 6:
+                        return email
+                        valid_mail = False
+                    else:
+                        print("Podaj poprawny adres e-mail")
+
+            email = valid_email_insert()
+
+            print(f"Twoje dane to:"
+                  f"\nImię: {name}"
+                  f"\nNazwisko: {surname}"
+                  f"\nNume telefonu: {phone}"
+                  f"\nE-mail: {email}")
+
+            accepting_data = input("Czy powyższe dane się zgadzają? (tak lub nie): ")
+            if accepting_data.lower() == "tak":
+                print("Potwierdzono")
+                return name, surname, phone, email
+                valid_choice = False
+            elif accepting_data.lower() == "nie":
+                print("Generuję formularz od nowa")
+            else:
+                print("Wprowadzono niepoprawne dane, wprowadź dane od nowa")
+
+    def confirming_imputed_data(desk_name, desk_type, user_beginning_date_str, user_end_date_str, total_value):
         while True:
             accepting_order = input("Potwierdź czy powyższe informacje się zgadzają (tak lub nie): ")
             if accepting_order.lower() == 'tak':
                 print("Potwierdzono")
                 name, surname, phone, email = user_data_input()
-                reservation_id = saving_clients_data(desk_name, user_beginning_date_str, user_end_date_str, name, surname, phone, email)
+                reservation_id = saving_clients_data(name, surname, phone, email, desk_name, desk_type, user_beginning_date_str, user_end_date_str, total_value)
                 print(f"Zapisano pomyślnie dane.")
                 print(f"Twoj numer rezerwacji to: {reservation_id}, który został przypisany do numeru telefonu: {phone}")
                 return reservation_id
@@ -149,7 +292,7 @@ def user_reservation_module():
                 print("Nie potwierdzono")
             else:
                 print("Wprowadzono niewłaściwą odpowiedź, wprowadź odpowiedź tak lub nie")
-    confirming_imputed_data(desk_name, user_beginning_date_str, user_end_date_str, total_value)
+    confirming_imputed_data(desk_name, desk_type, user_beginning_date_str, user_end_date_str, total_value)
     print("Do widzenia")
 
 if __name__ == "__main__":
