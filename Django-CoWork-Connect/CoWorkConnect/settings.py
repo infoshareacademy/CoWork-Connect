9aea1f7d2c090c8731526812c525a5b272a5f764
SebekMal <sebastian.malolepszy1@gmail.com>
2024-03-17 12:29:17 +0100

    Zmiany pliku settings

--- Django-CoWork-Connect/CoWorkConnect/settings.py
+++ Django-CoWork-Connect/CoWorkConnect/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j7bhfga74=ei_li4s-@9l$&dj*1xpm(@_3vojm*@$i3of2hbee'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
