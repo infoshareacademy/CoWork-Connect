1637b7a3b0a0091dd65cbe445beee67cdb13c5b6
SebekMal <sebastian.malolepszy1@gmail.com>
2024-03-24 13:38:52 +0100

    Dodanie podstron do strony głównej i wpisanie oferty biura.

--- Django-CoWork-Connect/coapp/urls.py
+++ Django-CoWork-Connect/coapp/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
+    path("offer/", views.offer, name="offer"),
+    path("avaliability/", views.avaliability, name="avaliability"),
+    path("pricelist/", views.pricelist, name="pricelist"),
+    path("reservation/", views.reservation, name="reservation"),
+    path("regulations/", views.regulations, name="regulations"),
+    path("contact/", views.contact, name="contact")
 ]
+
--- Django-CoWork-Connect/coapp/views.py
+++ Django-CoWork-Connect/coapp/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(response):
     return render(response, "coapp/home.html", {})
+
+def offer(response):
+    return render(response, "coapp/offer.html", {})
+
+def avaliability(response):
+    return render(response, "coapp/avaliability.html", {})
+
+def pricelist(response):
+    return render(response, "coapp/pricelist.html", {})
+def reservation(response):
+    return render(response, "coapp/reservation.html", {})
+def regulations(response):
+    return render(response, "coapp/regulations.html", {})
+def contact(response):
+    return render(response, "coapp/contact.html", {})
