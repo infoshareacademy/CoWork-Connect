e524088b0749e78cc5c495e624fdc3120e433f6c
Magda <magdalenakapka33@gmail.com>
2024-04-03 20:31:36 +0200

    Zaimportowano listę biurek do bazy projektu z desk.json, dodano opcję wyświetlania biurek w panelu admina

--- Django-CoWork-Connect/coapp/admin.py
+++ Django-CoWork-Connect/coapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Desk
 
-# Register your models here.
+@admin.register(Desk)
+class DeskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'desk_type', 'monitor', 'size', 'price', 'status')
+    list_filter = ('monitor', 'size', 'status')
+    search_fields = ('name', 'desk_type')
