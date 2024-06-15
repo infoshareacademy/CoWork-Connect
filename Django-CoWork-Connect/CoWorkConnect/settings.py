e5b416a6e873d7d6ec7d4cabbea4fc760ac62907
SebekMal <sebastian.malolepszy1@gmail.com>
2024-06-15 19:12:46 +0200

    Dodano logo do belki głównej w pliku base.html, przeorganizowano belkę główną w pliku base.html, dodano więcej opcji tekstowych także do regulaminu w pliku terms.html

--- Django-CoWork-Connect/CoWorkConnect/settings.py
+++ Django-CoWork-Connect/CoWorkConnect/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -130,6 +129,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "coapp/static",
+]
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
